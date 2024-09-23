c72bbfb59aaa6cb3f4d488113ba5fbcee2fce7d1
Rahul <rahullodhi39182@gmail.com>
2024-09-24 00:53:26 +0530

    react router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,22 @@
+import { lazy, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import LazyResult from 'postcss/lib/lazy-result'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+
+
+
+    </>
+  )
+}
+
+export default App
